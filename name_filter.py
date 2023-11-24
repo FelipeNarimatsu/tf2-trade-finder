b637cfb4c7a38916c797a90767d5741222f9aa3e
FelipeNarimatsu <felipe-narimatsu@hotmail.com>
2023-11-24 19:42:29 -0300

    feat: Add STN and Backpack.tf scrapper
    Add the scrapper bot to gather price information about the items listed on
    item_names.txt
    Add name_filter.py to filter raw STNTrading HTML and generate the items_names.txt

---
+++ name_filter.py
@@ -0,0 +1,20 @@
+import os
+os.remove('items_names.txt') 
+
+f1 = open('items.txt', 'r') 
+f2 = open('items_names.txt', 'a+')
+
+
+full_text = f1.read()
+full_text = full_text.split('<span class="item-name">')
+
+first_line_jump = True
+for line in full_text:
+    if(first_line_jump != True):
+        item_name = line.split('</span>',1)[0]
+        print(item_name)
+        f2.write(item_name+'\n')
+    first_line_jump = False
+
+f1.close()
+f2.close()
